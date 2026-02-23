67c988920540b05f9527ee1b6713e7d112e3d4ff
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-02-23 17:34:31 +0000

    aula 4

---
+++ calculadoraIMC.js
@@ -0,0 +1,20 @@
+function calculoIMC(peso, altura){
+    let imc = peso / (altura ** 2);
+
+    console.log("Seu IMC é, " + imc);
+
+    if (imc < 18,5){
+        console.log("Clasificação: Abaixo do peso");
+    }
+    else if (imc >= 18,5 && imc <= 24,9){
+        console.log("Classificação: Peso normal");
+    }
+    else if(imc >= 25 && imc <= 29,9){
+        console.log("Classificação: Sobrepeso");
+    }
+    else {
+        console.log("Clasificação: Obesidade");
+    }
+
+}
+calculoIMC(84, 1.84)
