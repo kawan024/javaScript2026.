cdc5e24d72dbc6fffaf813fcf62cdbad23d170ee
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-03-25 13:02:04 +0000

    cabei ja

---
+++ vetorFor1.js
@@ -0,0 +1,11 @@
+const notas = [7.5, 8.2, 9.6, 3.2];
+let soma = 0;
+
+for (i =0; i < notas.length; i++){
+    soma = soma + notas[i];
+}
+
+const média = soma / notas.length;
+
+console.log("A soma das notas é: " + soma.toFixed(2));
+console.log("A média do aluno é: " + Media.toFixed(2));
---
+++ vetorFor2.js
@@ -0,0 +1,8 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i = 0; i< numeros.length; i++){
+    if (Numeros[i] % 2 === 0){
+    console.log("O número " + numeros[i] + "é par!")
+         
+    }
+}
