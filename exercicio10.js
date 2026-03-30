d8c47da01d7324ffa9e1aeebbfe85f5b1985fb32
kawan024 <kawan.rodrigues.santos@escola.pr.gov.br>
2026-03-30 17:22:10 +0000

    acabei de corrigir

---
+++ exercicio10.js
@@ -0,0 +1,6 @@
+let saldo = 100;
+let saque = 120;
+id (saque <= saldo) {
+    saldo -= saque;}
+else }
+console.log("Saldo insuficiente");}
---
+++ exercicio6.js
@@ -0,0 +1,6 @@
+let valor = 20;
+if (valor === "20"){
+    console.log("Opção 1");
+}
+else {console.log("Opção 2"); 
+}
